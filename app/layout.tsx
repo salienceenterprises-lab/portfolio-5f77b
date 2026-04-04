7ef3306f1c7fce9905d2d03589111bb10a33e739
Salience Enterprises <salienceenterprises@gmail.com>
2026-04-04 15:32:25 -0400

    Update app/layout.tsx via Portfolio SaaS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
 import './globals.css';
+export const metadata = {
+  title: 'ABDULLAH SYED',
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
